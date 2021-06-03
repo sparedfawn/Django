911177fe44c6b05d88d46316be607455156d6611
Krzysztof <k.kalinowski2000@gmail.com>
2021-06-03 23:30:28 +0200

    Procfile and requirements

--- Project/urls.py
+++ Project/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('', include('mainapp.urls')),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
